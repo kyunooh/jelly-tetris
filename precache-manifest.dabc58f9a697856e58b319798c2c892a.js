ec31ff5e2de08eadd667c418823b9cb61e3e9c2e
jelly <chm073@gmail.com>
2019-04-30 22:35:20 +0900

    Updates

---
+++ precache-manifest.dabc58f9a697856e58b319798c2c892a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15cd780487a7ea0a1986247eaef91ccf",
+    "url": "/jelly-tetris/index.html"
+  },
+  {
+    "revision": "8ddd3d56ca8339523f13",
+    "url": "/jelly-tetris/static/css/main.9ed03034.chunk.css"
+  },
+  {
+    "revision": "3d8497e9e22c766be293",
+    "url": "/jelly-tetris/static/js/2.bbd4dcac.chunk.js"
+  },
+  {
+    "revision": "8ddd3d56ca8339523f13",
+    "url": "/jelly-tetris/static/js/main.b75f338e.chunk.js"
+  },
+  {
+    "revision": "294c62e609eeb59573b6",
+    "url": "/jelly-tetris/static/js/runtime~main.d1a7a9cd.js"
+  }
+]);
--- src/playview/GameOver.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import "./GameOver.scss";
-
-export default () => <div id="game-over">Game Over</div>;
