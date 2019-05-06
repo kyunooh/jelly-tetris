45294e4e0f5908bb6e21f895f85dd31698c5ae75
jelly <chm073@gmail.com>
2019-05-06 21:12:46 +0900

    Add bgm

--- src/information/InformationWindow.js
+++ src/information/InformationWindow.js
@@ -28,6 +28,11 @@ const InformationWindow = props => {
 
       <div id="removed-lines">Lines: {props.removedLines}</div>
       <div id="levels">Level: {props.levels}</div>
+      <div id="audio-panel">
+        <audio controls autoPlay={true} volume="0.2" loop>
+          <source src="/tetris.ogg" type="audio/ogg; codecs=vorbis"/>
+        </audio>
+      </div>
       <button id="prevent-reset" style={{ width: "0px", height: "0px" }} />
       <button onClick={props.reset}>Reset</button>
     </div>
